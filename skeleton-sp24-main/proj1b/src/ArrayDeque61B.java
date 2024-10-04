19aab95c85cce1fca17bf511ca05c6d55cfd5d1a
PAPERSHEEP <2250766585@qq.com>
2024-10-05 00:17:37 +0800

    Project1b

---
+++ skeleton-sp24-main/proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,129 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+    private T[] array;
+    private int size;
+    private int maxSize;
+    private int nextFirst, nextLast;
+
+    public ArrayDeque61B() {
+        maxSize = 8; // Initial max size to 8.
+        array = (T[]) new Object[maxSize];
+        size = 0;
+        nextFirst = 4;
+        nextLast = 5;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    @Override
+    public void addFirst(T x) {
+        if (size == maxSize) resizeUp();
+
+        array[nextFirst] = x;
+        nextFirst = Math.floorMod(nextFirst - 1, maxSize);
+        size++;
+    }
+
+    @Override
+    public void addLast(T x) {
+        if (size == maxSize) resizeUp();
+
+        array[nextLast] = x;
+        nextLast = Math.floorMod(nextLast + 1, maxSize);
+        size++;
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+
+        int index = Math.floorMod(nextFirst + 1, maxSize);
+        for (int i = 0; i < size; i++) {
+            returnList.add(array[index]);
+            index = Math.floorMod(index + 1, maxSize);
+        }
+        return returnList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) return null;
+        return array[Math.floorMod(nextFirst + 1 + index, maxSize)];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) return null;
+
+        while (size <= maxSize * 0.25) {
+            resizeDown();
+        }
+        T first = get(0);
+        nextFirst = Math.floorMod(nextFirst + 1, maxSize);
+        size--;
+        return first;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()) return null;
+
+        while (size <= maxSize * 0.25) {
+            resizeDown();
+        }
+        T last = get(size - 1);
+        nextLast = Math.floorMod(nextLast - 1, maxSize);
+        size--;
+        return last;
+    }
+
+    private void resizeUp() {
+        maxSize *= 2; // Double the size.
+        T[] newArray = (T[]) new Object[maxSize];
+
+        // Copy the elements to new array.
+        int index = Math.floorMod(nextFirst + 1, size);
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[index];
+            index = Math.floorMod(index + 1, size);
+        }
+        nextFirst = Math.floorMod(-1, maxSize);
+        nextLast = Math.floorMod(size, maxSize);
+        array = newArray;
+    }
+
+    private void resizeDown() {
+        maxSize /= 2; // Halve the size.
+        T[] newArray = (T[]) new Object[maxSize];
+
+        int index = Math.floorMod(nextFirst + 1, size);
+        for (int i = 0; i < size; i++) {
+            newArray[i] = array[index];
+            index = Math.floorMod(index + 1, size);
+        }
+        nextFirst = Math.floorMod(-1, maxSize);
+        nextLast = Math.floorMod(size, maxSize);
+        array = newArray;
+    }
+}
+
+
--- skeleton-sp24-main/proj1b/tests/ArrayDeque61BTest.java
+++ skeleton-sp24-main/proj1b/tests/ArrayDeque61BTest.java
@@ -10,14 +10,111 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BTest {
 
-//     @Test
-//     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
-//     void noNonTrivialFields() {
-//         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
-//                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-//                 .toList();
-//
-//         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-//     }
+     @Test
+     @DisplayName("ArrayDeque61B has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque61B.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
 
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
+
+    @Test
+    public void testAddFirstAndLast() {
+        ArrayDeque61B<String> al = new ArrayDeque61B<>();
+        assertThat(al.size()).isEqualTo(0);
+        al.addLast("a");
+        al.addLast("b");
+        al.addFirst("c");
+        al.addLast("d");
+        al.addLast("e");
+        al.addFirst("f");
+        al.addLast("g");
+        al.addLast("h");
+
+        assertThat(al.toList()).containsExactly("f", "c", "a", "b", "d", "e", "g", "h").inOrder();
+    }
+
+    @Test
+    public void testResize() {
+        ArrayDeque61B<Integer> a1 = new ArrayDeque61B<>();
+        a1.addFirst(2);
+        a1.addFirst(3);
+        a1.addLast(9);
+        a1.addFirst(7);
+        a1.addLast(6);
+        a1.addLast(5);
+        a1.addLast(11);
+        a1.addLast(4);
+        a1.addLast(15);
+        a1.addFirst(17);
+
+        assertThat(a1.size()).isEqualTo(10);
+        assertThat(a1.toList()).containsExactly(17,7,3,2,9,6,5,11,4,15).inOrder();
+    }
+
+    @Test
+    public void testisEmpty(){
+        ArrayDeque61B<Integer> a1 = new ArrayDeque61B<>();
+        assertThat(a1.isEmpty()).isTrue();
+        a1.addFirst(1);
+        assertThat(a1.isEmpty()).isFalse();
+    }
+
+    @Test
+    public void testGet(){
+        ArrayDeque61B<Integer> a1 = new ArrayDeque61B<>();
+        a1.addFirst(2);
+        a1.addFirst(3);
+        a1.addLast(9);
+        a1.addFirst(7);
+        a1.addLast(6);
+        a1.addLast(5);
+        a1.addLast(11);
+        a1.addLast(4);
+        a1.addLast(15);
+        a1.addFirst(17);
+
+        assertThat(a1.get(1)).isEqualTo(7);
+    }
+
+    @Test
+    public void testRemoveFirstAndLast() {
+        ArrayDeque61B<String> a1 = new ArrayDeque61B<>();
+        a1.addFirst("a");
+        a1.addFirst("c");
+        a1.addLast("f");
+        a1.addFirst("t");
+        a1.addLast("h");
+        a1.addLast("q");
+        a1.addLast("s");
+        a1.addLast("b");
+        a1.addLast("l");
+        a1.addFirst("k");
+
+        assertThat(a1.toList()).containsExactly("k","t","c","a","f","h","q","s","b","l").inOrder();
+        assertThat(a1.removeFirst()).isEqualTo("k");
+        assertThat(a1.removeLast()).isEqualTo("l");
+        assertThat(a1.toList()).containsExactly("t","c","a","f","h","q","s","b").inOrder();
+        assertThat(a1.size()).isEqualTo(8);
+
+        ArrayDeque61B<String> a2 = new ArrayDeque61B<>();
+        assertThat(a2.removeFirst()).isEqualTo(null);
+    }
+
+    @Test
+    public void testResizeDown(){
+        ArrayDeque61B<String> a1 = new ArrayDeque61B<>();
+        for(int i = 0;i<20;i++){
+            a1.addLast("a");
+        }
+
+        assertThat(a1.getMaxSize()).isEqualTo(32);
+        for(int i = 0;i < 18;i++){
+            a1.removeFirst();
+        }
+        assertThat(a1.getMaxSize()).isEqualTo(8);
+        assertThat(a1.size()).isEqualTo(2);
+    }
 }
